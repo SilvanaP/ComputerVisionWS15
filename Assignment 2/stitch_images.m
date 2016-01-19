24721268bd7af07121494f87388bbcb09ffb3a5c
dornik <mail.dominik.bauer@gmail.com>
2016-01-19 16:50:22 +0100

    * fixed stitch_images.m (homogenize ftw!)

--- Assignment 2/stitch_images.m
+++ Assignment 2/stitch_images.m
@@ -1,12 +1,12 @@
-function [] = stitch_images(path, prefix, type, startIndex, endIndex, output);
-%usage: stitch_images('ass4_data','officeview','.jpg',1,5,0)
-%       stitch_images('ass4_data','campus','.jpg',1,5,0)
-%       stitch_images('ass4_data/rankweil','rankweil','.jpg',1,7,0)
+function [] = stitch_images(path, prefix, type, endIndex, output);
+%usage: stitch_images('ass4_data','officeview','.jpg',5,0)
+%       stitch_images('ass4_data','campus','.jpg',5,0)
+%       stitch_images('ass4_data/rankweil','rankweil','.jpg',5,0)
+%note:  image index has to start with 1
 %---------
 %path... e.g. ass4_data
 %prefix... e.g. campus or officeview
 %type... image type, e.g. .jpg
-%startIndex... index of first image e.g. 1
 %endIndex... index of last image e.g. 5
 %output... = 1 -> generate output for report
 
@@ -17,6 +17,7 @@ if ~strcmp(last_char,'/')
 end
 
 %load images
+startIndex = 1;
 firstImage = imread([path, prefix, num2str(startIndex), type]);
 [height,width,channels] = size(firstImage);
 images = zeros(height,width,channels,endIndex,'uint8');
@@ -32,6 +33,7 @@ border(1,1:end) = 1;
 border(end,1:end) = 1;
 border(1:end,1) = 1;
 border(1:end,end) = 1;
+%alpha@position = distance to nearest non-zero pixel in border
 alpha = uint8(bwdist(border));
 alphas = repmat(alpha,1,1,3,endIndex);
 
@@ -46,6 +48,7 @@ centerIndex = floor((endIndex-startIndex)/2 + startIndex);
 % 2) compute transformations to center image from pair-wise homography's
 %tranformations and inverse-transformations
 transformToCenter = repmat(eye(3),1,1,endIndex);
+
 %mappings of i in [start,center-1] -> centerIndex
 for i = startIndex : centerIndex-1
     transformToCenter(:,:,i) = homographiesToNext(i).tdata.T; %e.g. i=1 -> 1,2
@@ -56,6 +59,7 @@ for i = startIndex : centerIndex-1
         end
     end
 end
+
 %mappings of i in [end,center+1] -> centerIndex
 for i = endIndex : -1 : centerIndex+1
     transformToCenter(:,:,i) = homographiesToNext(i-1).tdata.Tinv; %e.g. i=5 -> 5,4
@@ -66,23 +70,25 @@ for i = endIndex : -1 : centerIndex+1
         end
     end
 end
+
 %mapping centerIndex -> centerIndex stays identity
 
 % 3) compute size of output panorama
-%define corner points
-LT = [0,0,1];
-LB = [0,height,1];
-RT = [width,0,1];
+%define corner points (homogenous)
+LT = [1,1,1];
+LB = [1,height,1];
+RT = [width,1,1];
 RB = [width,height,1];
+
 points = repmat([LT;LB;RT;RB]',1,1,endIndex);
+
 %transform points
 for i = startIndex : endIndex
-%     plot(points(1,:,i),points(2,:,i),'bo')
-%     hold on
-    points(:,:,i) = transformToCenter(:,:,i) * points(:,:,i);
-%     plot(points(1,:,i),points(2,:,i),'ro')
-%     hold off
+    points(:,:,i) = transformToCenter(:,:,i)' * points(:,:,i);
 end
+%homogenize
+points = points./repmat(points(3,:,:),3,1,1);
+
 %find min/max x/y coordinates
 minValues = min(points, [], 2);
 maxValues = max(points, [], 2);
@@ -92,36 +98,13 @@ minY = min(minValues(2,:,:));
 maxX = max(maxValues(1,:,:));
 maxY = max(maxValues(2,:,:));
 
-%%%TODO%%%
-% find correct min/max values
-% (see values below "debug" in next for-loop)
-
 % 4) transform images
-xyScale = [1,1];
-
 xData = [minX, maxX];
 yData = [minY, maxY];
 
 transformedImages = [];
 transformedAlphas = [];
-for i = startIndex : endIndex
-    %debug: hardcoded; generated values crop too much
-    if strcmp(prefix,'officeview')
-        %approximate values for "officeview"
-        xData = [-700,1500];
-        yData = [-200,1300];
-    end
-    if strcmp(prefix,'campus') 
-        %approximate values for "campus"
-        xData = [-600,1000];
-        yData = [-200,700];
-    end
-    if strcmp(prefix,'rankweil') 
-        %approximate values for "campus"
-        xData = [-3000,3500];
-        yData = [-1000,1500];
-    end
-    
+for i = startIndex : endIndex   
     %transform images
     imageToTransform = images(:,:,:,i);
     tForm = maketform('projective', transformToCenter(:,:,i)); %create transformation structure
@@ -152,9 +135,5 @@ if output == 1
     set(fBase,'name','Stitching without blending','numbertitle','off')
     imshow(base);
 end
-
-% %debug
-% int16(xData)
-% int16(yData)
 
 end
